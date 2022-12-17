05fafc3e37ee722ca147869400c0a816f2d9a7bb
Miguel Ángel Sánchez-Medina Camacho <91555636+Miguel-Angel-Sanchez@users.noreply.github.com>
2022-12-17 11:38:54 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -141,19 +141,29 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
-/* SWIPER DE LOS PROYECTOS GITHUB */
+// Este es el método que vamos a llamar
+// cada vez que se detecte una intersección
+function onScrollEvent(entries, observer) {
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            var attributes = entry.target.attributes;
+            var src = attributes['data-src'].textContent;
+            entry.target.src = src;
+            entry.target.classList.add('visible');
+        }
+    });
+}
 
-let swiper = new Swiper('.portfolio__container', {
-    cssMode: true,
-    loop:true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
-    },
-    mousewheel: true,
-    keyboard: true,
+// Utilizamos como objetivos todos los
+// elementos que tengan la clase lazyLoad,
+// que vimos en el HTML de ejemplo.
+var targets = document.querySelectorAll('.lazyLoad');
+
+// Instanciamos un nuevo observador.
+var observer = new IntersectionObserver(onScrollEvent);
+
+// Y se lo aplicamos a cada una de las
+// imágenes.
+targets.forEach(function(entry) {
+    observer.observe(entry);
 });
